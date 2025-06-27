03ed8ad7036144ace66520c2c3fd0fa04e437f8e
arifwahh <arifwahh@gmail.com>
2025-06-27 12:40:38 +0700

    fix dynamic asset link

--- halaman/petugas/template/footer.php
+++ halaman/petugas/template/footer.php
@@ -16,9 +16,9 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="../../../assets/administrator/plugins/jquery/jquery.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="../../../assets/administrator/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -26,47 +26,47 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../../assets/administrator/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="../../../assets/administrator/plugins/chart.js/Chart.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="../../../assets/administrator/plugins/sparklines/sparkline.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="../../../assets/administrator/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../../../assets/administrator/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="../../../assets/administrator/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="../../../assets/administrator/plugins/moment/moment.min.js"></script>
-<script src="../../../assets/administrator/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/moment/moment.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="../../../assets/administrator/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="../../../assets/administrator/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="../../../assets/administrator/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../../assets/administrator/dist/js/adminlte.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/dist/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../../assets/administrator/dist/js/demo.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../../../assets/administrator/dist/js/pages/dashboard.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/dist/js/pages/dashboard.js"></script>
 <!-- DataTables  & Plugins -->
-<script src="../../../assets/administrator/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../../assets/administrator/plugins/jszip/jszip.min.js"></script>
-<script src="../../../assets/administrator/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../../assets/administrator/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../../assets/administrator/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/jszip/jszip.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- Javascript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="../../../assets/js/wizard.js"></script>
+<script src="<?= $base_url ?>/assets/js/wizard.js"></script>
 <!-- Page specific script -->
 <script>
   $(function () {
@@ -98,7 +98,7 @@ $(document).ready(function(){
      });  
      $('#submit').click(function(){            
           $.ajax({  
-               url:"../../../proses/tambahalldata.php",  
+               url:"<?= $base_url ?>/proses/tambahalldata.php",  
                method:"POST",  
                data:$('#add_keluhankunjungan').serialize(),  
                success:function(data)  
--- halaman/petugas/template/header.php
+++ halaman/petugas/template/header.php
@@ -3,6 +3,12 @@
 	if($_SESSION['status']!="login"){
 		header("location:../login.php?pesan=belum_login");
 	}
+  // config.php atau di bagian atas file PHP
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $base_url = '/sistemalihrm';  // nama folder project di htdocs
+} else {
+    $base_url = 'https://sistemalihrm.awmorecreative.com';  // root domain langsung, misal https://sistemalihrm.awmorecreative.com
+}
 	?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,36 +20,36 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../../assets/administrator/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/summernote/summernote-bs4.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../../assets/administrator/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/administrator/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
-  <link rel="stylesheet" href="../../../assets/css/wizard.css">
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/css/wizard.css">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="../../../assets/administrator/dist/img/logobakti.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="<?= $base_url ?>/assets/administrator/dist/img/logobakti.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -95,7 +101,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="../../../assets/administrator/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <img src="<?= $base_url ?>/assets/administrator/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Brad Diesel
@@ -111,7 +117,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="../../../assets/administrator/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src="<?= $base_url ?>/assets/administrator/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   John Pierce
@@ -127,7 +133,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="../../../assets/administrator/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src="<?= $base_url ?>/assets/administrator/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Nora Silvester
@@ -188,7 +194,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="../../../assets/administrator/dist/img/logobakti.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= $base_url ?>/assets/administrator/dist/img/logobakti.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Puskesmas Kaliwates</span>
     </a>
 
@@ -197,7 +203,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../../../assets/administrator/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= $base_url ?>/assets/administrator/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo $_SESSION['nama_pengguna'];?></a>

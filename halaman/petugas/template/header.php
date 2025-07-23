3fc8304506b00c0fcb56da724871be74315d0ad4
arifwahh <arifwahh@gmail.com>
2025-07-23 21:22:19 +0700

    adding title

--- halaman/petugas/template/header.php
+++ halaman/petugas/template/header.php
@@ -59,6 +59,9 @@ if ($_SERVER['HTTP_HOST'] == 'localhost') {
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
+       <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Sistem Alih Media & Pemusnahan RM</a>
+      </li>
     </ul>
 
     <!-- Right navbar links -->
